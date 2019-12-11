dc013b6f5fbe38ca6e0618ca87d4641e6dcde2d5
alyxstewart <alystewart2@gmail.com>
2019-12-11 12:49:05 -0800

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -44,8 +44,17 @@ def calculate_total (price):
     # Return total
     return grand_total
 
-# Prompt user to see if they want a printed receipt
+#Function to prompt user to see if they want a printed receipt
+
 def want_receipt (receipt):
     yes_or_no = input ("Would you like to print a receipt? (Y or N) : ")
     if "Y" or "y":
-        print receipt
+        order_number = receipt_file_name
+        #Order_number is whatever the UPC is (might have to change) 
+        receipt = open (receipt_file_name.txt, "w")
+        receipt.write(data)
+        # Change data to whatever we name the UPC file return info
+        receipt.close()
+        
+    else:
+        print ("No Receipt")
